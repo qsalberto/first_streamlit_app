cf43a36a52014387397cfa5495e4ddf8fd440980
qsalberto <139975819+qsalberto@users.noreply.github.com>
2023-07-21 17:08:00 +0800

    Update streamlit_app.py
    remove stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ except:
 # streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+# streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
